ba15f7d54988a8e4231663d987bacb7827752cdf
binbin1760 <569279172@qq.com>
2023-04-02 21:09:19 +0800

    upload

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <Starjob></Starjob>
+</template>
+
+<script setup lang="ts">
+import Starjob from './components/Starjob.vue';
+</script>
---
+++ src/components/Starjob.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="h-screen">
+    <img src="@/assets/施工.png" alt="">
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
