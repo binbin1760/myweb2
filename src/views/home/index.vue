97ea2c3e8379f4d0de97605229dde3e3b6c6bec2
lier_gitee <1932898614@qq.com>
2023-04-26 21:32:19 +0800

    feat:change

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home-page">
     <div class="menu">
-      <sidebar v-for="(item, index) in siderBar" :key="index" :siderBar="item"></sidebar>
+      <sidebar
+        v-for="(item, index) in siderBar"
+        :key="index"
+        :siderBar="item"
+      ></sidebar>
     </div>
     <div class="router-page">
       <router-view></router-view>
@@ -10,14 +14,14 @@
 </template>
 
 <script setup lang="ts">
-import sidebar from "@/components/menu/index.vue"
+import sidebar from "@/components/menu/index.vue";
 // sidebar iconfont import
-import dashBoard from "@/assets/iconfont/主页.png"
+import dashBoard from "@/assets/iconfont/主页.png";
 const siderBar = [
   { key: "主页", routerUrl: "xxxx", url: dashBoard },
+  { key: "主页12312313", routerUrl: "xxxx", url: dashBoard },
   { key: "主页", routerUrl: "xxxx", url: dashBoard },
-  { key: "主页", routerUrl: "xxxx", url: dashBoard },
-]
+];
 </script>
 
 <style scoped>
