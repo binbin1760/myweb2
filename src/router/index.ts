0475ba5e7ba09ccd5ef38f1a11c8250a1cc4e02d
binbin1760 <569279172@qq.com>
2023-04-28 17:21:28 +0800

    feat: router list

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 // 路由懒加载
 const Home =()=>import('@/views/home/index.vue')
+const Dashboard=()=>('@/views/home/dashboard/index.vue')
 const routes=[
-  {path:"/home",name:"主页",componet:Home,redirect: '/'},
+  {path:"/home",name:"主页",component:Home,children:[]},
+  {path:"/home",redirect: '/'}
 ]
+const router=createRouter({
+  history:createWebHashHistory(),
+  routes
+})
+
+export default router
