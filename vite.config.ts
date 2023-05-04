9bd5bafa0834c8b3fc8da560a56f9e7577f4409a
binbin1760 <569279172@qq.com>
2023-05-04 17:26:54 +0800

    chore: add  pulgins

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Component from "unplugin-vue-components/vite"
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
 import path from "path"
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Component({
+    dts:true,
+    dirs:['src/components'],
+    resolvers:[NaiveUiResolver()]
+  }),
+    AutoImport({
+      resolvers: [NaiveUiResolver()],
+      imports:['vue','vue-router'],
+      dts:true
+    })
+],
   resolve:{
     alias:{
       '@': path.resolve(__dirname, './src')
