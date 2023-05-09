341ce0b78a6a49972317dedae4b2a6e0cbbd7794
binbin1760 <569279172@qq.com>
2023-05-09 17:51:56 +0800

    feat: add some font

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <div class="home-left">
-      <Card></Card>
+      <Card :trends="tsteData"></Card>
     </div>
     <div class="home-right">
       <Search></Search>
@@ -12,6 +12,13 @@
 <script setup lang="ts">
 import { Card } from '@/components';
 import { Search } from './components';
+import imgCard from "@/assets/Avator-多老A梦.png"
+const tsteData={
+    title: "需要加点动画",
+    imgurl: imgCard,
+    time:'2023-12-11 12:52:00',
+    intro: '我还不知道怎么去弄iconfont的svg。。。。。',
+}
 </script>
 
 <style scoped>
