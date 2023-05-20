e10911392486f5467cc92453d4b4366fdd0483d2
binbin1760 <569279172@qq.com>
2023-05-20 23:57:37 +0800

    fix: change ui

---
+++ src/views/control/index-control/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="">测试</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped></style>
---
+++ src/views/control/work-benches/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="">工作台</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped></style>
