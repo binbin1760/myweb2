9dd5b53853ad04564aff2aa4368c8b1d7488885f
binbin1760 <569279172@qq.com>
2023-06-09 22:30:08 +0800

    feat: wrokbenches

--- src/views/control/components/index.ts
+++ src/views/control/components/index.ts
@@ -1,9 +1,7 @@
 import SideMenu from './side-menu/index.vue'
 
-import Crunmbs from './crumbs/index.vue'
-
 import Draw from './drawer/index.vue'
 
-export { SideMenu, Crunmbs, Draw }
+export { SideMenu, Draw }
 
 export * from '../components'
